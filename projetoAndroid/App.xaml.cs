e60d2b766f6fd712bff9ed3713578e83ecfac1c8
Cavichia-D-Santos <thiagogamer10000gmail.com>
2025-05-21 17:51:22 -0300

    Adição: Configs. do DB

--- projetoAndroid/App.xaml.cs
+++ projetoAndroid/App.xaml.cs
@@ -1,7 +1,25 @@
-﻿namespace projetoAndroid
+﻿using projetoAndroid.Helpers;
+
+namespace projetoAndroid
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelpers _db;
+        public static SQLiteDatabaseHelpers Db
+        {
+            get
+            {
+                if(_db == null)
+                {
+                    string path = Path.Combine(Environment.GetFolderPath
+                        (Environment.SpecialFolder.LocalApplicationData),
+                        "banco_sqlite_sysceapp.db3");
+
+                    _db = new SQLiteDatabaseHelpers(path);
+                }
+                return _db;
+            }
+        }
         public App()
         {
             InitializeComponent();
