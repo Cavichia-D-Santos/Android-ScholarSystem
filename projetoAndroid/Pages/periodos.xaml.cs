77778056ab06ba10ada8d7f2093b3b1c0f37458c
Cavichia-D-Santos <thiagogamer10000gmail.com>
2025-05-21 17:52:07 -0300

    Features: Adição, listagem e remoção de dados

--- projetoAndroid/Pages/periodos.xaml.cs
+++ projetoAndroid/Pages/periodos.xaml.cs
@@ -1,19 +1,70 @@
-namespace projetoAndroid.Pages;
+using projetoAndroid.Model;
+using System.Collections.ObjectModel;
+//using projetoAndroid.Views;
 
-public partial class periodos : ContentPage
+namespace projetoAndroid.Pages
 {
-	public periodos()
+	public partial class periodos : ContentPage
 	{
-		InitializeComponent();
-	}
+		ObservableCollection<Periodo> lista = new ObservableCollection<Periodo>();
+		public periodos()
+		{
+			InitializeComponent();
+			lstPeriodos.ItemsSource = lista;
+		}
+		//NAVEGAÇÃO
+		private async void PaginaAdicionarPeriodo_Clicked(object sender, EventArgs e)
+		{
+			await Navigation.PushAsync(new adicionarPeriodo());
+		}
 
-	private async void OnClickPaginaAdicionarPeriodo(object sender, EventArgs e)
-	{
-		await Navigation.PushAsync(new adicionarPeriodo());
-	}
+		private async void PaginaEditarPeriodo_Clicked(object sender, EventArgs e)
+		{
+			await Navigation.PushAsync(new editarPeriodo());
+		}
+		//DADOS
+		protected async override void OnAppearing()
+		{
+			List<Periodo> tmp = await App.Db.GetAll();
+			lista.Clear();
 
-    private async void OnClickPaginaEditarPeriodo(object sender, EventArgs e)
-    {
-        await Navigation.PushAsync(new editarPeriodo());
-    }
+			foreach(Periodo periodo in tmp)
+			{
+				lista.Add(periodo);
+			}
+		}
+
+		private async void MenuAlterar_Clicked(object sender, EventArgs e)
+		{
+			//Periodo p = new Periodo();
+		}
+
+		private async void MenuRemover_Clicked(object sender, EventArgs e)
+		{
+			MenuItem selecionado = sender as MenuItem;
+			Periodo p = selecionado.BindingContext as Periodo;
+
+			bool confirma = await DisplayAlert("ATENÇÃO", "Confirma a remoção?", "Sim", "Não");
+
+			if(confirma == true)
+			{
+				await App.Db.Delete(p.Id);
+				lista.Remove(p);
+				await DisplayAlert("SUCESSO", "Item removido.", "OK");
+			}
+		}
+
+		private async void scbarPeriodo_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			string q = e.NewTextValue;
+			lista.Clear();
+
+			List<Periodo> tmp = await App.Db.Search(q);
+
+			foreach(Periodo periodo in tmp)
+			{
+				lista.Add(periodo);
+			}
+		}
+	}
 }
